cac5dc16ca90e774218231c31e1b4c21ea42d6b4
yjinJun <jyj94444@gmail.com>
2021-05-02 16:41:31 +0900

    Delete [JYJ]Majority Element.py

--- [JYJ]Majority Element.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        numset = set(nums)
-        for i in numset:
-            if nums.count(i) > len(nums)/2:
-                return i
