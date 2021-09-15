7dcca88c6f4312930f98bdbf37a9012a05a2cad7
Choi asher <tahun970@gmail.com>
2021-09-16 08:24:20 +0900

    Update SchoolTest.java

--- [CTH]solution/easy/SchoolTest.java
+++ [CTH]solution/easy/SchoolTest.java
@@ -1,42 +1,23 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
-        int[][] map = new int[n][m];
+        int[][] map = new int[m + 1][n + 1];
+        for (int i = 0; i < puddles.length; i++) {
+            map[puddles[i][0]][puddles[i][1]] = -1;
+        }
+        
+        map[1][1] = 1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == 0 || j == 0) {
-                    map[i][j] = 1;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (map[i][j] == -1) {
+                    map[i][j] = 0;
+                    continue;
                 }
+                if (i != 1) map[i][j] += map[i - 1][j] % 1000000007;
+                if (j != 1) map[i][j] += map[i][j - 1] % 1000000007;
             }
         }
 
-        for (int[] puddle : puddles) {
-           if (puddle.length == 2) {
-                int x = puddle[0];
-                int y = puddle[1];
-                map[y-1][x-1] = -1;
-            }
-        }
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 1; j < m; j++) {
-                int up = map[i - 1][j];
-                int left = map[i][j - 1];
-                int cur = map[i][j];
-
-                if (cur >= 0) {
-                    if (up < 0 && left < 0) {
-                        map[i][j] = -1;
-                    } else if (up < 0) {
-                        map[i][j] = left;
-                    } else if (left < 0) {
-                        map[i][j] = up; 
-                    } else {
-                        map[i][j] = (left + up) % 1000000007;
-                    }
-                }
-            }
-        }
-        return map[n - 1][m - 1] % 1000000007;
+        return map[m][n] % 1000000007;
     }
 }
